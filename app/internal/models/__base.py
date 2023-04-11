e37cbd99fa45c831b341cdf9a6dbaf67220bafec
Mateus Cabral <jmateuscabral@gmail.com>
2023-04-11 11:15:18 -0300

    Atualizando models base para registrar update

--- app/internal/models/__base.py
+++ app/internal/models/__base.py
@@ -7,4 +7,4 @@ class Base(DeclarativeBase):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     created: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, nullable=True)
-    updated: Mapped[str] = mapped_column(DateTime, nullable=True)
+    updated: Mapped[str] = mapped_column(DateTime, onupdate=datetime.now, nullable=True)
